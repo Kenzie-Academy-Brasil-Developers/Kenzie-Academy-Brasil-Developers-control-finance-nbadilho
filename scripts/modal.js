1d419d5d5bb6cbe0ed142ef90cc8611ca6d006bf
Natalia <nataliabadilho@hotmail.com>
2022-09-28 20:20:53 -0300

    entrega

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,1 +1,14 @@
 /* Desenvolva sua lógica aqui */
+let modal=document.querySelector(".modal")
+
+
+let registrar=document.querySelector("[data-botao-registar")
+registrar.addEventListener("click",function(event){
+    modal.classList.toggle("hidden")
+})
+
+let sairECancelar=document.querySelectorAll("[data-modal-button]")
+let arraySair=[...sairECancelar]
+arraySair.forEach(item=>item.addEventListener("click",function(event){
+    modal.classList.toggle("hidden")
+}))
